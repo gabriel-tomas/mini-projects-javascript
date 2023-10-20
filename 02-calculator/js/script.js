574b588fc0aab3b0de933cd8cba08e23da9f35d3
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-10-20 20:33:36 -0300

    update math funcs

--- 02-calculator/js/script.js
+++ 02-calculator/js/script.js
@@ -1,7 +1,4 @@
 function mainScope() {
-    const resultButton = document.querySelector("#result-button");
-    const resultCampo = document.querySelector("#result-campo > p");
-    const multButton = document.querySelector("#mult-button");
     const button0 = document.querySelector("#button-0");
     const button1 = document.querySelector("#button-1");
     const button2 = document.querySelector("#button-2");
@@ -12,13 +9,16 @@ function mainScope() {
     const button7 = document.querySelector("#button-7");
     const button8 = document.querySelector("#button-8");
     const button9 = document.querySelector("#button-9");
+    const equalButton = document.querySelector("#equal-button");
+    const resultCampo = document.querySelector("#result-campo > p");
+    const multButton = document.querySelector("#mult-button");
+    const minusButton = document.querySelector("#minus-button");
+    const addButton = document.querySelector("#add-button");
+
 
     function addEventsSigns() {
-        multButton.addEventListener("mousedown", function () {
-            Calculadora.getSign("*");
-        });
-        resultButton.addEventListener("mousedown", function() {
-            Calculadora.getExpression(resultCampo);
+        equalButton.addEventListener("mousedown", function() {
+            Calculadora.postExpression("=");
         });
         button0.addEventListener("mousedown", function() {
             Calculadora.postExpression("0");
@@ -50,34 +50,79 @@ function mainScope() {
         button9.addEventListener("mousedown", function() {
             Calculadora.postExpression("9");
         })
+        multButton.addEventListener("mousedown", function () {
+            Calculadora.postExpression("*");
+        });
+        minusButton.addEventListener("mousedown", function () {
+            Calculadora.postExpression("-");
+        });
+        addButton.addEventListener("mousedown", function () {
+            Calculadora.postExpression("+");
+        });
     }
 
     const Calculadora = {
         resultado: 0,
-        expression: "",
-
-        //getSign(element) {
-        //    if(element == "mult") {
-
-        //    }
-        //},
-
-        //multNumber() {
-
-        //}
-
-        getExpression(resultElement) {
-            console.log(resultElement.innerHTML);
-            return resultElement.innerHTML;
-        },
+        displayNumbers: "",
+        signTimes: 0,
+        signs: ["*", "-", "+"],
+        actualSignOperation: null,
+        numbers: [],
 
         postExpression(text) {
-            console.log(typeof text);
-            if(typeof Number(text) == "number") {
-                this.expression += text;
-                resultCampo.innerHTML = this.expression;
+            console.log(text);
+            if(!isNaN(Number(text))) {
+                this.displayNumbers += text;
+                resultCampo.innerHTML = this.displayNumbers;
+            } else if(this.signs.indexOf(text) != -1) {
+                this.actualSignOperation = text;
+                
+                this.numbers.push(Number(this.displayNumbers));
+                this.displayNumbers = "";
+            } else if(text === "=") {
+                this.numbers.push(Number(this.displayNumbers));
+    
+                this.displayNumbers = "";
             }
+
+            console.log(this.numbers);
+            console.log(this.actualSignOperation);
+
+            if(this.numbers.length === 2) {
+                if(this.actualSignOperation === "+") {
+                    this.resultado = this.add(this.numbers[0], this.numbers[1]);
+                    resultCampo.innerHTML = this.resultado;
+                    this.numbers = [this.resultado];
+                    console.log(this.numbers);
+                } else if(this.actualSignOperation === "-") {
+                    this.resultado = this.minus(this.numbers[0], this.numbers[1]);
+                    resultCampo.innerHTML = this.resultado;
+                    this.numbers = [this.resultado];
+                    console.log(this.numbers);
+                } else if(this.actualSignOperation === "*") {
+                    this.resultado = this.mult(this.numbers[0], this.numbers[1]);
+                    resultCampo.innerHTML = this.resultado;
+                    this.numbers = [this.resultado];
+                    console.log(this.numbers);
+                }
+            }
+        },
+
+        add(num1, num2) {
+            console.log(num1 + num2);
+            return num1 + num2;
+        },
+
+        minus(num1, num2) {
+            console.log(num1 - num2);
+            return num1 - num2;
+        },
+
+        mult(num1, num2) {
+            console.log(num1 * num2);
+            return num1 * num2;
         }
+
     }
 
     addEventsSigns();
