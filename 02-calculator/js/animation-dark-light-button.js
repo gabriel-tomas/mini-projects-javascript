9b3fbe57df42e6792994ec3f0b794c2824f14182
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-10-21 21:57:14 -0300

    dark and white mode

---
+++ 02-calculator/js/animation-dark-light-button.js
@@ -0,0 +1,38 @@
+function DarkLightButton() {
+    const buttonDarkLight = document.querySelector("#light-dark-mode-button");
+    const selectorDarkLight = document.querySelector("#light-dark-mode-button >  .selector-dark-light");
+    let buttonActived = false;
+
+    function activeDesactive() {
+        if(!buttonActived) {
+            selectorDarkLight.style.right = "0px";
+            buttonDarkLight.style.backgroundColor = "black";
+            selectorDarkLight.style.backgroundColor = "white";
+            buttonActived = true;
+            changeTheme("dark");
+        } else {
+            selectorDarkLight.style.right = "calc(100% - 40px)";
+            selectorDarkLight.removeAttribute("right");
+            buttonDarkLight.style.backgroundColor = "white";
+            selectorDarkLight.style.backgroundColor = "black";
+            buttonActived = false;
+            changeTheme("white");
+        }
+    }
+
+    function changeTheme(theme) {
+        if(theme === "dark") {
+            document.documentElement.style.setProperty("--color0", "rgb(33, 33, 33)")
+            document.documentElement.style.setProperty("--color1", "rgb(117, 117, 117)")
+            document.documentElement.style.setProperty("--text-color", "rgb(234, 234, 234)")
+        } else if(theme == "white") {
+            document.documentElement.style.setProperty("--color0", "rgb(61, 61, 61)")
+            document.documentElement.style.setProperty("--color1", "rgb(228, 228, 228)")
+            document.documentElement.style.setProperty("--text-color", "#000")
+        }
+    }
+    
+    buttonDarkLight.addEventListener("mousedown", activeDesactive);
+}
+
+DarkLightButton();
