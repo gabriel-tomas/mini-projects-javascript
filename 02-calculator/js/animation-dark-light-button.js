68983f453d5a8d36a51500e0d65eedcd57b95f3e
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-10-22 20:38:00 -0300

    header styles

--- 02-calculator/js/animation-dark-light-button.js
+++ 02-calculator/js/animation-dark-light-button.js
@@ -11,7 +11,7 @@ function DarkLightButton() {
             buttonActived = true;
             changeTheme("dark");
         } else {
-            selectorDarkLight.style.right = "calc(100% - 40px)";
+            selectorDarkLight.style.right = "calc(100% - 33px)";
             selectorDarkLight.removeAttribute("right");
             buttonDarkLight.style.backgroundColor = "white";
             selectorDarkLight.style.backgroundColor = "black";
@@ -25,10 +25,12 @@ function DarkLightButton() {
             document.documentElement.style.setProperty("--color0", "rgb(33, 33, 33)")
             document.documentElement.style.setProperty("--color1", "rgb(117, 117, 117)")
             document.documentElement.style.setProperty("--text-color", "rgb(234, 234, 234)")
+            document.documentElement.style.setProperty("--color2", "rgb(78, 78, 78)")
         } else if(theme == "white") {
             document.documentElement.style.setProperty("--color0", "rgb(61, 61, 61)")
             document.documentElement.style.setProperty("--color1", "rgb(228, 228, 228)")
             document.documentElement.style.setProperty("--text-color", "#000")
+            document.documentElement.style.setProperty("--color2", "rgb(244, 244, 244)")
         }
     }
     
