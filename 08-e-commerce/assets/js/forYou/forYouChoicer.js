60d7c8066227dd8f91d79d2de8c8a42f00b2cdde
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-11-19 20:53:53 -0300

    container interests choicer finished

--- 08-e-commerce/assets/js/forYou/forYouChoicer.js
+++ 08-e-commerce/assets/js/forYou/forYouChoicer.js
@@ -1,11 +1,7 @@
 (() => {
     class ForYouChoicer {
         createContainerChoicer() { 
-            const divContainerChoicer = document.createElement("div");
-            divContainerChoicer.classList.add("container-choicer-for-you");
-            const containerOptions = document.createElement("div");
-            containerOptions.classList.add("container-options");
-            divContainerChoicer.appendChild(containerOptions);
+            const{divContainerChoicer, containerOptions} =  createNoRepeatEl();
 
             productsMethods.getAllCategories((category) => {
                 category.forEach(value => {
@@ -34,9 +30,54 @@
             function addChoicerToBody(containerChoicer) {
                 document.body.appendChild(containerChoicer);
             }
-        }
 
-        
+            function createNoRepeatEl() {
+                const divContainerChoicer = document.createElement("div");
+                divContainerChoicer.classList.add("container-choicer-for-you");
+
+                const divH1 = document.createElement("div");
+                divH1.classList.add("container-title");
+                const h1 = document.createElement("h1");
+                h1.innerText = "Choose your interests";
+                //h1.classList.add("");
+                divH1.appendChild(h1);
+
+                const btnSaveInterests = document.createElement("button");
+                btnSaveInterests.innerText = "Confirm";
+                btnSaveInterests.classList.add("btn-save-interests", "primary-button", "radius-total", "button");
+                btnSaveInterests.addEventListener("click", getSelectedInterests);
+                const containerBtnSave = document.createElement("div");
+                containerBtnSave.classList.add("container-btn-save");
+                containerBtnSave.appendChild(btnSaveInterests);
+
+                const containerOptions = document.createElement("div");
+                containerOptions.classList.add("container-options");
+                divContainerChoicer.appendChild(divH1);
+                divContainerChoicer.appendChild(containerOptions);
+                divContainerChoicer.appendChild(containerBtnSave);
+
+                return {divContainerChoicer, containerOptions};
+            }
+
+            function getSelectedInterests() {
+                const inputsCheckbox = document.querySelectorAll(".input-choicer-category");
+                let selectedItems = [];
+
+                inputsCheckbox.forEach(value => {
+                    if(value.checked) {
+                        selectedItems.push(value.parentElement.querySelector(".category-name").innerText);
+                    }
+                })
+
+                localStorageSave("interestedItems", selectedItems);
+                destroyContainerChoicer();
+            }
+
+            function destroyContainerChoicer() {
+                const divContainerChoicer = document.querySelector(".container-choicer-for-you");
+                divContainerChoicer.remove();
+            }
+        }
     }
 
     const forYouChoicer = new ForYouChoicer();
---
+++ 08-e-commerce/assets/js/localStorageSaverAndGet.js
@@ -0,0 +1,8 @@
+function localStorageSave(key, value) {
+    if(typeof key !== "string") throw("Key must be string");
+    if(key.length < 3) throw("Key must be greater than 2");
+    value = JSON.stringify(value);
+    localStorage.setItem(key, value);
+    console.log("Key:", key, "| value", value, "| > saved ");
+    console.log(`Type of key: ${typeof key}, Type of value ${typeof value}`);
+}
