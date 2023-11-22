c9136106f0979235f05b93c06249357c46614a13
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-11-22 00:34:31 -0300

    items na tela com links com href da página de produtos sendo enviado com parâmetros

--- 08-e-commerce/assets/js/forYou/forYou.js
+++ 08-e-commerce/assets/js/forYou/forYou.js
@@ -12,42 +12,48 @@
 
         create() {
             if(this.interests) {
-                this.createProductsCard(this.interests);
+                this.separateInterestItems(this.interests);
                 return;
             };
-            randomCategories().then(array => {
-                this.createProductsCard(array);
+            this.randomCategories().then(array => {
+                this.separateInterestItems(array);
             });
+        }
+
+        randomCategories() {
+            return new Promise((resolve, reject) => {
+                let arrayRandomCategories = [];
+                productsMethods.getAllCategories(categories => {
+                    for(let i = 0; i < 4; i++) {
+                        const randomValue = getRandom(categories.length);
+                        if(categories[randomValue]) arrayRandomCategories.push(categories[randomValue]);
+                    }
+                    resolve(arrayRandomCategories);
+                });
+                function getRandom(max) {
+                    return Math.round(Math.random() * (max - 0) + 0);
+                }
+            })
+        }
         
-            function randomCategories() {
-                return new Promise((resolve, reject) => {
-                    let arrayRandomCategories = [];
-                    productsMethods.getAllCategories(categories => {
-                        for(let i = 0; i < 4; i++) {
-                            const randomValue = getRandom(categories.length);
-                            if(categories[randomValue]) arrayRandomCategories.push(categories[randomValue]);
-                        }
-                        resolve(arrayRandomCategories);
-                    });
-                    function getRandom(max) {
-                        return Math.round(Math.random() * (max - 0) + 0);
+        separateInterestItems(array) {
+            for(let i in array) {
+                productsMethods.setCategoryProducts(array[i], e => {
+                    const rand = Math.round(Math.random() * (e.products.length - 0) + 0);
+                    for(let product of e.products) {
+                        this.addProductCard(product);
                     }
                 })
             }
         }
 
-        createProductsCard(array) {
-            console.log(array);
-            for(let interests of array) {
-                productsMethods.setCategoryProducts(interests, e => {
-                    console.log(e.products);
-                })
-            }
+        addProductCard(productObj) {
+            const forYouProducts = document.querySelector(".for-you-products");
+            const product = new createProductCard(productObj).create();
+            forYouProducts.appendChild(product);
         }
-
-        
     }
 
-    const forYou = new ForYou().create();
-    
+    // starter
+    new ForYou().create();
 })();
---
+++ 08-e-commerce/assets/js/productCard.js
@@ -0,0 +1,73 @@
+class createProductCard {
+    constructor(product) {
+        //console.log(product);
+        this.thumbnail = product.thumbnail;
+        this.title = product.title;
+        this.price = product.price;
+        this.discountPercentage = product.discountPercentage;
+        this.id = product.id;
+    }
+
+    create() {
+        const divParent = this.divParent;
+        const containerAndThumb = this.containerAndThumb;
+        const bottomInfo = this.bottomInfos;
+
+        divParent.appendChild(containerAndThumb);
+        divParent.appendChild(bottomInfo);
+
+        return divParent;
+    }
+
+    get divParent() {
+        const div = document.createElement("div");
+        div.setAttribute("id", this.id);
+        div.classList.add("container-product");
+        
+        const a = document.createElement("a");
+        a.classList.add("product-link");
+        a.setAttribute("href", `product.html?product;id=${this.id}`);
+
+        div.append(a);
+
+        return a;
+    }
+
+    get containerAndThumb() {
+        const div = document.createElement("div");
+        div.classList.add("container-thumbnail");
+
+        const img = document.createElement("img");
+        img.setAttribute("src", this.thumbnail);
+        div.appendChild(img);
+
+        return div;
+    }
+
+    get bottomInfos() {
+        const div = document.createElement("div");
+        div.classList.add("container-bottom-infos");
+
+        const divTitle = document.createElement("div");
+        divTitle.classList.add("container-title");
+        const spanTitle = document.createElement("span");
+        spanTitle.innerText = this.title;
+        divTitle.appendChild(spanTitle);
+
+        const divPriceAndOthers = document.createElement("div");
+        divPriceAndOthers.classList.add("container-price-and-others");
+        const spanPrice = document.createElement("span");
+        spanPrice.classList.add("span-price");
+        spanPrice.innerText = `$ ${this.price}`;
+        const discount = document.createElement("span");
+        discount.classList.add("discount");
+        discount.innerText = `${this.discountPercentage} OFF`;
+        divPriceAndOthers.appendChild(spanPrice);
+        divPriceAndOthers.appendChild(discount);
+
+        div.appendChild(divTitle);
+        div.appendChild(divPriceAndOthers);
+
+        return div;
+    }
+}
