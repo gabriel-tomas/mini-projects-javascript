58e0d74d5a181936849ae6eecafb8183dc265d24
gabriel-tomas <gabriel.tomas.com@gmail.com>
2023-12-12 01:54:17 -0300

    css for home page

--- 12-notes/src/controllers/home.js
+++ 12-notes/src/controllers/home.js
@@ -1,3 +1,5 @@
 exports.homePage = (req, res, next) => {
-    res.render("index");
+    res.render("index", {
+        welcomeWords: ["ideias", "pensamentos", "projetos", "compromissos", "senhas", "contas", "datas", "horários", "observações", "comentários", "mensagens", "emails", "endereços", "listas", "dicas", "preços", "produtos"]
+    });
 }
